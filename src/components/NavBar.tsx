8624f3f651cc0c70e07a87ccbb77c55dcfbf442e
gilmarxd <gilmar_custodio_@hotmail.com>
2021-02-13 14:14:52 -0300

    fix navbar z-index and size

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,22 +1,24 @@
 import React from 'react'
 
+import cartIcon from '../icons/cart-icon.svg'
+
 export default function NavBar() {
 	return (
 		<nav
 			className={
-				'fixed top-0 w-full h-16 bg-blue-500 text-white flex items-center justify-between px-4'
+				'fixed top-0 w-full h-1 z-50 shadow bg-blue-500 text-black flex items-center justify-between px-4'
 			}
 		>
 			<div className='flex gap-2'>
-				<a href='/' className='text-2xl font-bold hover:text-black'>
+				<a href='/' className='text-2xl font-bold hover:text-white'>
 					Gammerce
 				</a>
-				<small className='hidden sm:block self-end text-gray-200 '>
+				<small className='hidden sm:block self-end text-gray-700'>
 					a pseudo gamming ecommerce
 				</small>
 			</div>
-			<button className='h-10 w-10 rounded-full bg-indigo-50 text-blue-500 appearance-none focus:outline-none'>
-				cart
+			<button className='h-12 w-12 flex justify-center items-center rounded-full bg-black text-blue-500'>
+				<img className='h-8 w-8' alt='Cart' src={cartIcon} />
 			</button>
 		</nav>
 	)
