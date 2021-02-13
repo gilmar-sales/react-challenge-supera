a6bd0b59a09657efb278f96be81ca433e3c88672
gilmarxd <gilmar_custodio_@hotmail.com>
2021-02-13 14:21:04 -0300

    add products to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+
+import GameCard from '../components/GameCard'
+import { Game } from '../@types/game'
 
 export default function Home() {
+	const [games, setGames] = useState<Game[]>([])
+	const [loadingPage, setLoadingPage] = useState(true)
+
 	useEffect(() => {
 		fetch('products.json', {
 			headers: {
@@ -9,12 +15,28 @@ export default function Home() {
 			},
 		})
 			.then((response) => response.json())
-			.then((products) => console.log(products))
+			.then((products) => {
+				setGames(products)
+				setLoadingPage(false)
+			})
 	}, [])
 
 	return (
-		<div>
-			<h1 className='text-2xl'>React Challenge Supera</h1>
+		<div className='p-4'>
+			<h1 className='text-2xl mb-4'>React Challenge Supera</h1>
+			<div className='grid grid-cols-12 gap-4'>
+				{loadingPage
+					? [...Array(8)].map((_, index) => (
+							<div className='col-span-12 sm:col-span-4 md:col-span-3'>
+								<GameCard loading />
+							</div>
+					  ))
+					: games.map((game, index) => (
+							<div className='col-span-12 sm:col-span-4 md:col-span-3'>
+								<GameCard key={index} game={game} />
+							</div>
+					  ))}
+			</div>
 		</div>
 	)
 }
