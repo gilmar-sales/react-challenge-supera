5ce6991b03d5ee0c9f075bc900a126e24bdaa7ac
gilmarxd <gilmar_custodio_@hotmail.com>
2021-02-13 11:29:49 -0300

    fetch products data and print for debug

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Home() {
+	useEffect(() => {
+		fetch('products.json', {
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			},
+		})
+			.then((response) => response.json())
+			.then((products) => console.log(products))
+	}, [])
+
 	return (
 		<div>
 			<h1 className='text-2xl'>React Challenge Supera</h1>
