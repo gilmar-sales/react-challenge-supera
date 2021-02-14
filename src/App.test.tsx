170c89f03d6824bd2feb01884f950838fdedc4ea
gilmarxd <gilmar_custodio_@hotmail.com>
2021-02-14 14:30:09 -0300

    update app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import App from './App'
+import { ShopCartProvider } from './contexts/ShopCartContext'
 
-test('renders learn react link', () => {
-	render(<App />)
-	const linkElement = screen.getByText(/learn react/i)
-	expect(linkElement).toBeInTheDocument()
+test('it renders without errors', () => {
+	render(
+		<ShopCartProvider>
+			<App />
+		</ShopCartProvider>
+	)
 })
