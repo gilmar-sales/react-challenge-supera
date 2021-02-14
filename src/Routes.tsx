999f8e01167de804c4477fa73ce5dc6c263fd4d2
gilmarxd <gilmar_custodio_@hotmail.com>
2021-02-13 21:03:57 -0300

    add navbar and cartdrawer in routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
+import CartDrawer from './components/CartDrawer'
+import NavBar from './components/NavBar'
+
 import HomePage from './pages/Home'
 
 export default function Routes() {
 	return (
 		<BrowserRouter>
+			<NavBar />
+			<CartDrawer />
 			<Switch>
 				<Route path='/' exact component={HomePage} />
 			</Switch>
